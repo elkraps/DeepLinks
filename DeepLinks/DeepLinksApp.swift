b9e0e59bad90975e4c07e1f1cab937c563a350f2
elkraps <dobroisreal@mail.ru>
2023-07-21 09:16:23 +0300

    Initial Commit

---
+++ DeepLinks/DeepLinksApp.swift
@@ -0,0 +1,17 @@
+//
+//  DeepLinksApp.swift
+//  DeepLinks
+//
+//  Created by Vlad Dzirko on 21.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct DeepLinksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
